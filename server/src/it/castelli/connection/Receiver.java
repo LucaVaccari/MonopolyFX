426de20b12d8d92718e439f908a6b04620e3f31e
YehorCherednychenko <57537299+YehorCherednychenko@users.noreply.github.com>
2021-03-10 11:54:28 +0100

    Partially implemented Connection, Sender and Receiver

--- server/src/it/castelli/connection/Receiver.java
+++ server/src/it/castelli/connection/Receiver.java
@@ -1,5 +1,49 @@
 package it.castelli.connection;
 
-public class Receiver
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class Receiver implements Runnable
 {
+	private Socket connectionSocket;
+	private User user;
+	private byte[] receivedMessage = new byte[2048];
+	private boolean isRunning = true;
+
+	public Receiver(Socket connectionSocket, User user)
+	{
+		this.connectionSocket = connectionSocket;
+		this.user = user;
+	}
+
+	public void interrupt()
+	{
+		isRunning = false;
+		try
+		{
+			connectionSocket.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run()
+	{
+		try (InputStream inStream = connectionSocket.getInputStream())
+		{
+			while (isRunning)
+			{
+
+			}
+		}
+		catch (IOException e)
+		{
+			System.out.println("The connection has been interrupted");
+		}
+
+	}
 }
--- utils/src/it/castelli/connection/Sender.java
+++ utils/src/it/castelli/connection/Sender.java
@@ -1,5 +1,24 @@
 package it.castelli.connection;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Sender
 {
+	private InputStream inputStream;
+	private OutputStream outputStream;
+	public Sender(Socket connectionSocket)
+	{
+		try
+		{
+			inputStream = connectionSocket.getInputStream();
+			outputStream = connectionSocket.getOutputStream();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
