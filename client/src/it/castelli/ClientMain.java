7cf8fc0dde9653de1e8d5d1380aec93b8119a210
Andrea Ferraboli <andreaferraboli2004@gmail.com>
2021-03-10 11:20:34 +0100

    Scenemanager

--- client/src/it/castelli/ClientMain.java
+++ client/src/it/castelli/ClientMain.java
@@ -1,9 +1,21 @@
 package it.castelli;
 
-public class ClientMain
+import it.castelli.gui.scene.SceneManager;
+import it.castelli.gui.scene.SceneType;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientMain extends Application
 {
 	public static void main(String[] args)
 	{
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		SceneManager.getInstance().setPrimaryStage(primaryStage);
+		SceneManager.getInstance().showScene(SceneType.MAIN_MENU);
 	}
 }
---
+++ client/src/it/castelli/gui/scene/SceneType.java
@@ -0,0 +1,6 @@
+package it.castelli.gui.scene;
+
+public enum SceneType
+{
+    MAIN_MENU, LOBBY, BOARD, EXCHANGE, AUCTION, PROPERTY_VIEW, PLAYER_INFO
+}
