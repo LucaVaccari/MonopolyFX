2d67272a004c999323faf0fe3da9fd68c57a1cd8
YehorCherednychenko <57537299+YehorCherednychenko@users.noreply.github.com>
2021-03-24 12:34:12 +0100

    Created Auction methods,

--- utils/src/it/castelli/gameLogic/transactions/Auction.java
+++ utils/src/it/castelli/gameLogic/transactions/Auction.java
@@ -1,5 +1,41 @@
 package it.castelli.gameLogic.transactions;
 
+import it.castelli.gameLogic.Player;
+import it.castelli.gameLogic.contracts.Contract;
+
 public class Auction
 {
+	private Contract contract;
+	private int bestOfferProposed;
+	private Player player;
+
+	public Auction(Contract contract)
+	{
+		this.contract = contract;
+		bestOfferProposed = 10;
+		player = null;
+	}
+
+	public void offer(Player player, int offer)
+	{
+		if (bestOfferProposed < offer)
+		{
+			bestOfferProposed = offer;
+			this.player = player;
+		}
+	}
+
+	public void startAuction()
+	{
+		//TODO: start timer
+	}
+
+	private void endAuction()
+	{
+		if (player != null)
+		{
+			player.getContracts().add(contract);
+			//TODO: Print the winner of the auction
+		}
+	}
 }
