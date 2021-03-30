b26620e4b2815b3732330c3966c9ad43456561fb
NicolaVivante <57539526+NicolaVivante@users.noreply.github.com>
2021-03-30 13:39:33 +0200

    created Auction offer and Auction messages

---
+++ client/src/it/castelli/connection/messages/BoardClientMessage.java
@@ -0,0 +1,22 @@
+package it.castelli.connection.messages;
+
+import it.castelli.Game;
+import it.castelli.connection.Connection;
+import it.castelli.gameLogic.Player;
+import it.castelli.gameLogic.squares.Square;
+
+public class BoardClientMessage implements Message
+{
+    private Square[] board;
+
+    public BoardClientMessage(Square[] board)
+    {
+        this.board = board;
+    }
+
+    @Override
+    public void onReceive(Connection connection, Player player)
+    {
+        Game.getGameManager().setBoard(board);
+    }
+}
