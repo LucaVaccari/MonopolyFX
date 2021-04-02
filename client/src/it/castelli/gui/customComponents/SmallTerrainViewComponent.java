e7d50678dbf0cb50dc8982ddc1ee693354eaa8e1
LucaVaccari <lucavaccari03@gmail.com>
2021-04-02 15:52:24 +0200

    Added FXMLs (without implementation): auction, exchange, ownedTerrainView, playerInfo, smallTerrainView

---
+++ client/src/it/castelli/gui/customComponents/SmallTerrainViewComponent.java
@@ -0,0 +1,7 @@
+package it.castelli.gui.customComponents;
+
+import javafx.scene.layout.AnchorPane;
+
+public class SmallTerrainViewComponent extends AnchorPane // TODO: IMPLEMENT SMALL_TERRAIN_VIEW_COMPONENT
+{
+}
