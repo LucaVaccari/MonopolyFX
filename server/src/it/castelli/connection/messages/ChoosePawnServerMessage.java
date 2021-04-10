1a53a8631ea6109719b292ce82ecf9035daea5fd
Andrea Ferraboli <andreaferraboli2004@gmail.com>
2021-04-10 22:30:01 +0200

    PawnChoiceController refresh

--- server/src/it/castelli/connection/messages/ChoosePawnServerMessage.java
+++ server/src/it/castelli/connection/messages/ChoosePawnServerMessage.java
@@ -3,6 +3,7 @@ package it.castelli.connection.messages;
 import it.castelli.connection.Connection;
 import it.castelli.connection.ConnectionManager;
 import it.castelli.connection.GameConnectionManager;
+import it.castelli.gameLogic.Pawn;
 import it.castelli.gameLogic.Player;
 import it.castelli.serialization.Serializer;
 
@@ -14,7 +15,7 @@ public class ChoosePawnServerMessage implements Message
 	/**
 	 * The pawn URL that represent the image of the pawn
 	 */
-	private final String pawnURL;
+	private final Pawn pawn;
 
 	/**
 	 * The game code
@@ -24,19 +25,19 @@ public class ChoosePawnServerMessage implements Message
 	/**
 	 * Constructor for ChoosePawnServerMessage (do not use)
 	 *
-	 * @param pawnURL The pawn URL that represent the image of the pawn
+	 * @param pawn The pawn URL that represent the image of the pawn
 	 * @param gameCode The game code
 	 */
-	public ChoosePawnServerMessage(String pawnURL, int gameCode)
+	public ChoosePawnServerMessage(Pawn pawn, int gameCode)
 	{
-		this.pawnURL = pawnURL;
+		this.pawn = pawn;
 		this.gameCode = gameCode;
 	}
 
 	@Override
 	public void onReceive(Connection connection, Player player)
 	{
-		player.setPawnPath(pawnURL);
+		player.setPawn(pawn);
 		GameConnectionManager manager = ConnectionManager.getInstance().getGames().get(gameCode);
 		manager.sendAll(ServerMessages.UPDATE_PLAYERS_LIST_MESSAGE_NAME,
 		                Serializer.toJson(new UpdatePlayersListServerMessage(manager.getGameManager().getPlayers())));
