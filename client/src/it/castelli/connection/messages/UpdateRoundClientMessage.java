5fa9f467d21a309de436fe24c7233244fd7a1785
NicolaVivante <57539526+NicolaVivante@users.noreply.github.com>
2021-04-12 16:22:21 +0200

    Removed debug log in UpdateRoundClientMessage

--- client/src/it/castelli/connection/messages/UpdateRoundClientMessage.java
+++ client/src/it/castelli/connection/messages/UpdateRoundClientMessage.java
@@ -31,9 +31,6 @@ public class UpdateRoundClientMessage implements Message
 	public void onReceive(Connection connection, Player player)
 	{
 		Game.getGameManager().setCurrentRound(round);
-
-		System.out.println("Il giocatore (dal round) " + round.getCurrentActivePlayer() + " è in prigione? " + round.getCurrentActivePlayer().isInPrison());
-		System.out.println("Il giocatore (dal Game) " + Game.getPlayer().getName() + " è in prigione? " + Game.getPlayer().isInPrison());
 		Platform.runLater(() -> BoardController.getInstance().update());
 	}
 }
