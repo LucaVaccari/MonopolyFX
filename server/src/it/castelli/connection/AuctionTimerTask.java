0d7a0f7ad85b1478ef5ab6d455f2fe0f7b0813a3
NicolaVivante <57539526+NicolaVivante@users.noreply.github.com>
2021-04-20 10:36:39 +0200

    Added javadoc for AuctionTimerTask

--- server/src/it/castelli/connection/AuctionTimerTask.java
+++ server/src/it/castelli/connection/AuctionTimerTask.java
@@ -9,29 +9,55 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * The class handling the timer of the auction
+ */
 public class AuctionTimerTask extends TimerTask
 {
+	/**
+	 * The game code this auction is in
+	 */
 	private final int gameCode;
+
+	/**
+	 * The number of seconds after which the auction ends without any further offer
+	 */
 	private int repeats;
+
+	/**
+	 * The timer handling the auction
+	 */
 	Timer timer;
 
+	/**
+	 * Constructor for AuctionTimerTask
+	 *
+	 * @param repeats The number of seconds after which the auction ends without any further offer
+	 * @param gameCode The game code this auction is in
+	 */
 	public AuctionTimerTask(int repeats, int gameCode)
 	{
 		this.repeats = repeats;
 		this.gameCode = gameCode;
 	}
 
-	public void init()
+	/**
+	 * Starts a new countdown for the auction
+	 */
+	public void startAuction()
 	{
 		timer = new Timer();
 		timer.schedule(this, 0, TimeUnit.SECONDS.toMillis(1));
 	}
 
+	/**
+	 * At each step this method updates the players about the number of seconds left. If it becomes 0, the auction ends
+	 */
 	@Override
 	public void run()
 	{
 		if (repeats == 0)
-			stop();
+			endAuction();
 		else
 		{
 			ConnectionManager.getInstance().getGames().get(gameCode)
@@ -41,7 +67,10 @@ public class AuctionTimerTask extends TimerTask
 		repeats--;
 	}
 
-	public void stop()
+	/**
+	 * Ends the auction calling the GameManager method to assign the contract to the player
+	 */
+	public void endAuction()
 	{
 		timer.cancel();
 		System.out.println("Auction timer finished");
@@ -53,7 +82,10 @@ public class AuctionTimerTask extends TimerTask
 
 	}
 
-	public void interrupt()
+	/**
+	 * Ends the auction timer
+	 */
+	public void interruptAuctionTimer()
 	{
 		timer.cancel();
 	}
