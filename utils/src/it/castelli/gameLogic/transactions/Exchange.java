5166aae863fc9ee825413c2e0dd3d8fc310e6e00
YehorCherednychenko <57537299+YehorCherednychenko@users.noreply.github.com>
2021-04-20 10:33:29 +0200

    Added Javadoc for Exchange

--- utils/src/it/castelli/gameLogic/transactions/Exchange.java
+++ utils/src/it/castelli/gameLogic/transactions/Exchange.java
@@ -5,16 +5,47 @@ import it.castelli.gameLogic.Player;
 
 import java.util.Objects;
 
-
+/**
+ * Exchange class
+ */
 public class Exchange
 {
+	/**
+	 * A player who wants to exchange something
+	 */
 	private final Player player1;
+
+	/**
+	 * Another player who wants to exchange something
+	 */
 	private final Player player2;
+
+	/**
+	 * The asset to exchange of the first player
+	 */
 	private Asset asset1;
+
+	/**
+	 * The asset to exchange of the second player
+	 */
 	private Asset asset2;
+
+	/**
+	 * Indicate if the first player has accepted the exchange conditions
+	 */
 	private Boolean accepted1 = false;
+
+	/**
+	 * Indicate if the second player has accepted the exchange conditions
+	 */
 	private Boolean accepted2 = false;
 
+	/**
+	 * The constructor for the Exchange class
+	 *
+	 * @param player1 A player who wants to exchange something
+	 * @param player2 Another player who wants to exchange something
+	 */
 	public Exchange(Player player1, Player player2)
 	{
 		this.player1 = player1;
@@ -23,16 +54,32 @@ public class Exchange
 		asset2 = new Asset();
 	}
 
+	/**
+	 * Get the first player of the exchange
+	 *
+	 * @return A player who wants to exchange something
+	 */
 	public Player getPlayer1()
 	{
 		return player1;
 	}
 
+	/**
+	 * Get the second player of the exchange
+	 *
+	 * @return Another player who wants to exchange something
+	 */
 	public Player getPlayer2()
 	{
 		return player2;
 	}
 
+	/**
+	 * Change the exchange offer of a specific player
+	 *
+	 * @param player The player who updates his/er offer
+	 * @param newOffer The new offer
+	 */
 	public void changeAsset(Player player, Asset newOffer)
 	{
 		if (player.betterEquals(this.player1))
@@ -41,6 +88,11 @@ public class Exchange
 			asset2 = newOffer;
 	}
 
+	/**
+	 * Accept the exchange by a player
+	 *
+	 * @param player The player who wants to accept the exchange
+	 */
 	public void acceptExchange(Player player)
 	{
 		if (player.betterEquals(this.player1))
@@ -49,6 +101,11 @@ public class Exchange
 			accepted2 = true;
 	}
 
+	/**
+	 * Undo the accept action by a player
+	 *
+	 * @param player Player who has changed his/er mind
+	 */
 	public void undoAcceptExchange(Player player)
 	{
 		if (player.betterEquals(this.player1))
@@ -57,6 +114,13 @@ public class Exchange
 			accepted2 = false;
 	}
 
+	/**
+	 * End exchange
+	 *
+	 * @param player1 A player who has participated to the exchange
+	 * @param player2 Another player who has participated to the exchange
+	 * @param gameManager
+	 */
 	public void endExchange(Player player1, Player player2, GameManager gameManager)
 	{
 		asset1.removeFromPlayer(player1, gameManager);
@@ -65,21 +129,41 @@ public class Exchange
 		asset2.addToPlayer(player1, gameManager);
 	}
 
+	/**
+	 * Get if the first player has accepted the conditions of exchange
+	 *
+	 * @return Indicate if the first player has accepted the exchange conditions
+	 */
 	public Boolean getAccepted1()
 	{
 		return accepted1;
 	}
 
+	/**
+	 * Get if the second player has accepted the conditions of exchange
+	 *
+	 * @return Indicate if the second player has accepted the exchange conditions
+	 */
 	public Boolean getAccepted2()
 	{
 		return accepted2;
 	}
 
+	/**
+	 * Get the asset of the first player
+	 *
+	 * @return The asset to exchange of the first player
+	 */
 	public Asset getAsset1()
 	{
 		return asset1;
 	}
 
+	/**
+	 * Get the asset of the first player
+	 *
+	 * @return The asset to exchange of the second player
+	 */
 	public Asset getAsset2()
 	{
 		return asset2;
